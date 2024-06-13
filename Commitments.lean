70f2caac6e452a40b18abeb9281124e41b871d67
Ashley Blacquiere <ashley.blacquiere@nic.bc.ca>
2024-06-13 15:27:27 -0700

    bump to Lean 4.80

--- Commitments.lean
+++ Commitments.lean
@@ -1,3 +1,3 @@
--- This module serves as the root of the `Commitments` library.
--- Import modules here that should be built as part of the library.
-import «Commitments».Basic
+import Commitments.Basic
+import Commitments.elgamal
+import Commitments.test
