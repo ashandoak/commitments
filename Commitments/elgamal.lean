ea929ac36f4d6288ebbf6df5e58d1c69006dd752
Ashley Blacquiere <ashley.blacquiere@nic.bc.ca>
2024-07-14 18:39:48 -0700

    removed experimental code

--- Commitments/elgamal.lean
+++ Commitments/elgamal.lean
@@ -1,12 +1,12 @@
 import Mathlib
 
-namespace ElGamal
-
 variable (G : Type) [Fintype G] [Group G] -- Ensures Fintype and Group ensures nonempty 
 variable (q : ℕ) [NeZero q] -- No need for this to be declared prime; this will come through with the DDH assumption 
 variable (g : G) -- g is any fixed generator
 
 noncomputable section
+
+namespace ElGamal
 
 def uniform_grp : PMF G := PMF.uniformOfFintype G -- A uniform distribution giving all elementst of G probability 1/|G|. Both Group G and Fintype G required here.
 
@@ -14,64 +14,32 @@ theorem zmod_nonempty {t : ℕ} [NeZero t] : (ZMod.fintype t).elems.Nonempty := 
   have h' : 0 < (ZMod.fintype t).elems.card := by sorry
   sorry 
 
-def keygen : PMF (G × ZMod q) := -- This works when G, Type, Fintype, Group is provided as a variable
+-- This seems like the right direction
+def keygen : PMF (G × ZMod q) :=
 do
-  let h ← PMF.uniformOfFinset (ZMod.fintype q).elems zmod_nonempty
-  pure (g^h.val, h) 
-
-#check keygen
-#check keygen _ _ _
-#check keygen ?_ ..
-#check (keygen _ _ _).1 
-#check keygen G q g 
-
-def keygen' {G : Type} [Fintype G] [Group G] : PMF (G × ZMod q) := -- This "works" when G not defined as a variable, but can't resolve the final HPow
-do
-  let h ← PMF.uniformOfFinset (ZMod.fintype q).elems zmod_nonempty
-  pure (g^h, h) 
-
-#check keygen'
-#check keygen' _
-
-def keygen'' (g : G) : PMF (G × ZMod q) := -- This "works" when G not defined as a variable, but can't resolve the final HPow
-do
-  let h ← PMF.uniformOfFinset (ZMod.fintype q).elems zmod_nonempty
-  pure (g^h, h)
-
-#check keygen''
-#check keygen'' _ 
-#check keygen'' 3 _
-variable (m : H)
-#check keygen'' 3 m
-
-def keygen₁ : PMF (G × ZMod q) :=
-do
-  let h ← PMF.uniformOfFintype (ZMod q) -- Becasue ZMod has a Fintype instance? 
+  let h ← PMF.uniformOfFintype (ZMod q) -- Becasue ZMod has a Fintype instance?  TODO: Change h to s
   pure (g^h.val, h) -- Why does `val` work here? Because we're using do notation here, and the wrapped ZMod q is "assigned" to h, so h *isn't* a PMF
 
-#check keygen₁
-#check @keygen₁
-#print keygen₁
-
--- I don't think this is wise.
-def keygen₂ : PMF (G × ZMod q) :=
-do
-  let h ← PMF.ofMultiset _ _
-  pure (g^h.val, h) 
-
-
-
-def commit₁ (m : G) (h : G) : ((Zmod q) × G × G) :=
+def commit (m : G) (h : G) : PMF (ZMod q × G × G) :=
 do
   let r ← PMF.uniformOfFintype (ZMod q)
-  pure (r, g^r, m*h^r) -- pass h in rather than using def gen_h
+  pure (r, g^r.val, m*h^r.val) -- pass h in rather than using def gen_h
 
-def open₁ (m : G) (r : ZMod q) (c : G × G) : Prop := by
-  let c' := ⟨gen_g^r, m*gen_h^r⟩
+def verify (h : G) (m : G) (r : ZMod q) (c : G × G) : Prop := by
+  let c' := (g^r.val, m*h^r.val)   
   exact c' = c
 
+theorem correctness (m : G) (h : G) := -- TODO: Clean this up
+do
+  let c ← (commit₁ m h).1
+  verify₁ h m c.1 (c.2, c.3) := by
+  let r := c.1
+  let c' := (g^r.val, m*h^r.val)
+  exact c' = (c.2, c.3)
+
+
 lemma isBinding (c : G × G) (m m' : G) : m = m' := by
-  let r₁ := (commit₁ m).1   
+  let r₁ := (commit m).1   
   let c₂ := ⟨gen_g^r', m'*gen_h^r'⟩ 
   have hc₁c₂ : c₁ = c₂ := by sorry
   have hgen_g : c₁.1 = c₂.1 := by sorry
