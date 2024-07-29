eb0265a1eda3bbcca08a54a43964a87e1f9bdd76
Ashley Blacquiere <ashley.blacquiere@nic.bc.ca>
2024-07-29 14:50:23 -0700

    more fixes to main

--- Main.lean
+++ Main.lean
@@ -1,11 +1,18 @@
 import Commitments
 
+
+variable {G : Type} [Group G] [Fintype G] 
+variable {g : G}
+
 def main : IO Unit := do
   IO.println "Hello, world!"
 
+  let key := ElGamal.keygen
+  let h := key.1
 
-
---  let x' ← ElGamal.commit q' t k  
+  let q := 17
+  let m := "test"
+  let x' ← ElGamal.commit h _  
 
 
 
