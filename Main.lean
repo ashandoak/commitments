e633d2fbf281d6ff92bdf09d3d3ebfb1cc4acb99
Ashley Blacquiere <ashley.blacquiere@nic.bc.ca>
2024-07-29 14:46:34 -0700

    Modification to main.lean

--- Main.lean
+++ Main.lean
@@ -1,13 +1,11 @@
 import Commitments
 
-
 def main : IO Unit := do
   IO.println "Hello, world!"
 
-  let x' ← commit q' t k  
-  pure (x')
 
-  let x' ← commit q' t k  
-  pure (x').2
+
+--  let x' ← ElGamal.commit q' t k  
+
 
 
