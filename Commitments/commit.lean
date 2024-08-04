27c02564e5b250d139d8ee2a7aaab4bec6ad4152
Ashley Blacquiere <ashley.blacquiere@nic.bc.ca>
2024-08-04 15:13:02 -0700

    completed generic def for commit_verify

--- Commitments/commit.lean
+++ Commitments/commit.lean
@@ -1,8 +1,9 @@
--- TODO: Add abstract variables to represent commit verify and keygen
 -- TODO: commit_verify that returns ZMod 2
 -- TODO: Cross reference Lupo chapter 3
 
 import Mathlib
+
+noncomputable section
 
 variable {G : Type} [Group G] [Fintype G] 
          {q : ℕ} [NeZero q]
@@ -10,22 +11,10 @@ variable {G : Type} [Group G] [Fintype G]
 
 variable (keygen : PMF (G × ZMod q))
          (commit : G → G → PMF ((ZMod q) × G × G))
-         (verify : G → G → G × G → ZMod q → Prop)
+         (verify : G → G → (ZMod q) × G × G → Prop)
 
-
-def commit_verify (m : G) : PMF ℕ  :=
+def commit_verify' (m : G) : PMF Prop :=
 do
-  -- let h ← keygen.1 -- TODO: Why does this result in a metavariable?
-  let h := keygen >>= fun x => return x.1
-  let c :=
-  do
-    let h' ← h
-    commit h' m
-  let b :=
-  do
-    let h' ← h
-    let c' ← c
-    pure c'
-  return (if (verify h' m c'.2 c'.1 = true) then 1 else 0)
-
-
+  let h ← keygen >>= fun x => return x.1
+  let c ← commit h m
+  return verify h m c
