35bcf7773c970cedb8f43c9c038b9c943d7c6fa2
Ashley Blacquiere <ashley.blacquiere@nic.bc.ca>
2024-08-06 21:42:17 -0700

    removed unnecessary typeclasses; switched prop -> zmod 2

--- Commitments/commit.lean
+++ Commitments/commit.lean
@@ -1,19 +1,14 @@
--- TODO: commit_verify that returns ZMod 2
--- TODO: Cross reference Lupo chapter 3
-
 import Mathlib
 
 noncomputable section
 
-variable {G : Type} [Group G] [Fintype G] 
-         {q : ℕ} [NeZero q]
-         {g : G}
+variable {G : Type}  
 
-variable (keygen : PMF (G × ZMod q))
-         (commit : G → G → PMF ((ZMod q) × G × G))
-         (verify : G → G → (ZMod q) × G × G → Prop)
+variable (keygen : PMF (G × G))
+         (commit : G → G → PMF (G × G × G))
+         (verify : G → G → G × G × G → ZMod 2)
 
-def commit_verify (m : G) : PMF Prop :=
+def commit_verify (m : G) : PMF (ZMod 2) :=
 do
   let h ← keygen >>= fun x => return x.1
   let c ← commit h m
