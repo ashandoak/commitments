9b0e7c228b582f583638026bb6a4be77581d17ea
ashandoak <ash.blacquiere@pm.me>
2024-09-19 22:32:49 -0700

    modify types used in base commitment defs

--- Commitments/commit.lean
+++ Commitments/commit.lean
@@ -2,16 +2,9 @@ import Mathlib
 
 noncomputable section
 
-variable {G : Type}  
+-- Spaces for messages (M), commitments (C), opening values (O) and keys (K)
+variable {M C O K : Type}  
 
-variable (keygen : PMF (G × G))
-         (commit : G → G → PMF (G × G × G))
-         (verify : G → G → G × G × G → ZMod 2)
-
-def commit_verify (m : G) : PMF (ZMod 2) :=
-do
-  let h ← keygen >>= fun x => return x.1
-  let c ← commit h m
-  return verify h m c
-
-def commit_verify_correct : Prop := ∀ (m : G), commit_verify keygen commit verify m = return 1
+variable (setup : K)
+         (commit : K → M → C × O)
+         (verify : K → M → C × O → ZMod 2)
