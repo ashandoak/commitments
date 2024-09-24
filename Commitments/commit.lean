35a6a563791c70b4aeeb3662ba03d38541b4d22c
ashandoak <ash.blacquiere@pm.me>
2024-09-24 09:06:39 -0700

    add todos following meeting with B

--- Commitments/commit.lean
+++ Commitments/commit.lean
@@ -5,6 +5,12 @@ noncomputable section
 -- Spaces for messages (M), commitments (C), opening values (O) and keys (K)
 variable {M C O K : Type}  
 
-variable (setup : K)
-         (commit : K → M → C × O)
+variable (setup : PMF K)
+         (commit : K → M → PMF (C × O))
          (verify : K → M → C × O → ZMod 2)
+
+-- TODO add in commit_verify to be used in correctness
+-- TODO fix to make use of PMF and commiy_verify
+def correctness : Prop := ∀ (m : M), verify setup m (commit setup m) = 1 
+
+-- theorem correctness' :  ∀ (m : M), verify setup m (commit setup m) = 1 := sorry 
