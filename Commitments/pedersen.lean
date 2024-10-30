73ad161943b622d3e4bce7cb9864ecb1e160d358
ashandoak <ash.blacquiere@pm.me>
2024-10-29 22:33:04 -0700

    fix exponent type error

--- Commitments/pedersen.lean
+++ Commitments/pedersen.lean
@@ -10,7 +10,7 @@ noncomputable section
 -/
 
 
-variable {G : Type} [Fintype G] [Group G] [HPow G G G] [DecidableEq G] 
+variable {G : Type} [Fintype G] [Group G] [DecidableEq G] 
 variable (q : ℕ) [NeZero q] -- No need for this to be declared prime; this will come through with the DDH assumption
 variable {g : G} -- g is any fixed generator
 
@@ -22,12 +22,13 @@ do
   let h ← PMF.uniformOfFintype G 
   return h 
 
-def commit (h : G) (m : G) : PMF (G × ZMod q) :=
+def commit (h : G) (m : ZMod q) : PMF (G × ZMod q) :=
 do
   let r ← PMF.uniformOfFintype (ZMod q)
-  return (g^m * h^r.val, r)
-  
-def verify (h : G) (m : G) (c : (G × ZMod q)) : Prop :=
-  if c.1 = g^m * h^c.2.val then true else false
+  return (g^m.val * h^r.val, r)
+ 
+-- TODO Return ZMod 2
+def verify (h : G) (m : ZMod q) (c : (G × ZMod q)) : Prop :=
+  if c.1 = g^m.val * h^c.2.val then true else false
 
 
